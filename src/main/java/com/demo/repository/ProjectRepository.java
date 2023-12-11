1f7baae55beedac28ae578d358b459d82cdfd2f1
mibal_ua <9mohapx9@gmail.com>
2023-12-11 17:50:55 +0100

    Add repositories for entities

---
+++ src/main/java/com/demo/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.demo.repository;
+
+import com.demo.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
---
+++ src/main/java/com/demo/repository/SprintRepository.java
@@ -0,0 +1,11 @@
+package com.demo.repository;
+
+import com.demo.model.Sprint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+public interface SprintRepository extends JpaRepository<Sprint, Long> {
+}
